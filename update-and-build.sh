505ffa447febb1a192576818a5f63ef0a4b54e8a
Adam Thiede <me@adamthiede.com>
2023-05-22 15:24:18 -0500

    add kanshictl package, varlink, and some deps

--- update-and-build.sh
+++ update-and-build.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 git pull
+
 mydir=$(pwd)
+failfile='/tmp/alpine-pkgs-build-status'
+cat /dev/null > $failfile
+
+error() {
+	echo "ERROR $1" >> $failfile
+}
+
+geterror() {
+	grep ERROR $failfile
+	if [[ $? == 1 ]];then
+		exit 0
+	else
+		exit 1
+	fi
+}
+
 for dir in ./*/* ; do
 	cd $dir
-	abuild checksum
-	abuild -r
+	abuild checksum || error $(basename $dir)
+	abuild -r || error $(basename $dir)
 	cd $mydir
 done
+
+geterror
 
